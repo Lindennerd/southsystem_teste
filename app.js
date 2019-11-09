9b6c6676081ac8b1964a495dd193e5ddd34710b4
Luiz Paulo <lindennerd@gmail.com>
2019-11-08 23:57:46 -0300

    ajuste no limite do tamanho de upload

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
